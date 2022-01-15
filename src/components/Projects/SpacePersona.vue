86cc1ef91266d5be5de5a0866be2c4da1e421618
BonnyWang <bonny.y.gardy@gmail.com>
2022-01-15 17:10:00 -0500

    tried video component

--- src/components/Projects/SpacePersona.vue
+++ src/components/Projects/SpacePersona.vue
@@ -6,6 +6,10 @@
         <h2 class="projectSubTitle">
          Created for Origin Space
         </h2>
+        <video width="320" height="240" controls>
+            <source src="movie.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
     </span>
   
 </template>
