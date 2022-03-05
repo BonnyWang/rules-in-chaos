dbaa2d2f7118f7d68476b97656c371512f5c89ac
BonnyWang <bonny.y.gardy@gmail.com>
2022-03-04 19:48:19 -0500

    minor change

--- src/components/Projects/SpaceObserver.vue
+++ src/components/Projects/SpaceObserver.vue
@@ -7,7 +7,7 @@
             A Web Platform for Showing Data Obtained by Yangwang-1 Satellite from Origin Space Company
         </h2>
         <p>
-            For yunxi conference
+            The animation is based on the real orbit data of the satellite.
         </p>
         <br>
         <iframe width="60%" height="315" src="https://www.youtube.com/embed/Nzd00f4zRsA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
